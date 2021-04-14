6885584ac010b8192883808dc560c19fa33b4126
Deepak Tewatia <deepaktewatia2000@gmail.com>
2021-04-14 11:10:13 +0530

    Added Recycle_bin table and query of it - Database_connection.py

--- Database_Connection.py
+++ Database_Connection.py
@@ -5,13 +5,30 @@ import sqlite3 as sql
 # and do some operations over database table
 class Database:
 
-    #constructor
+    #constructor to make connections with database and create recycle bin table
     def __init__(self):
         self.connector = sql.connect("Testing.db")
         self.cur = self.connector.cursor()
 
+        
+    def create_table_recycle_bin(self):
+
+        recycle_bin_query = """ CREATE TABLE Recycle_bin(
+                                 Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                                 Data CHAR NOT NUll   
+                                );
+                            """
+
+        try:
+            self.cur.execute(recycle_bin_query)
+            self.connector.commit()
+        except Exception as e:
+            return e
+        else:
+            return True
+
     # to create table if not exists
-    def create_table(self):
+    def create_table_clip_data(self):
         #Query to create A Table inside Database
         query = """   CREATE TABLE Clip_data(
                             Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -29,20 +46,34 @@ class Database:
         else:
             return True
 
-    #to delete main table
-    def delete_table(self):
-        query = "DROP TABLE Clip_data;"
+    #to delete table's data specified by user 
+    def delete_data_inside_table(self, table_name):
 
-        self.cur.execute(query)
-        self.connector.commit()
+        delete_query = "DELETE FROM {};".format( table_name )
+
+        try:
+
+            self.cur.execute(delete_query)
+            self.connector.commit()
+        except Exception as e:
+            return e
+        else:
+            return True
 
     #to return connector object
     def return_connector(self):
         return self.connector
 
-
+    #to return cursor object
     def return_cursor(self):
         return self.cur
 
 
 
+if __name__ == "__main__":
+
+    db = Database()
+
+    db.create_table_clip_data()
+    db.create_table_recycle_bin()
+    # print(db.delete_data_inside_table("Clip_data"))
